a905d042fb4e0d451faefc86e49a5a320e17f570
Kate <diachukkateryna78@gmail.com>
2024-05-28 19:25:39 +0300

    resolved grammar issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 import { burgerMenu } from "./burgerMenu.js";
-import { handleFooter } from "./main-sliders/index-sliders.js";
-import { changingSliders } from "./main-sliders/changingSliders.js";
+import { handleFooter } from "./main-slides/index-slides.js";
+import { changingSlides } from "./main-slides/changingSlides.js";
 import { registerForm } from "./register.js";
 
 burgerMenu();
 handleFooter();
-let interval = () => { setInterval(changingSliders, 5000) };
+let interval = () => { setInterval(changingSlides, 5000) };
 interval();
 registerForm();
